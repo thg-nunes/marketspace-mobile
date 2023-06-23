ec05b2cebbbd5d5fb53fe85bf9cb92810c821155
thiago <desenvolvedor.nunes@gmail.com>
2023-06-23 09:25:51 -0300

    chore: config inicial do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: [
+    'prettier',
+    'prettier/react',
+    'airbnb-typescript',
+    '@react-native-community',
+    'prettier/@typescript-eslint'
+  ]
+}
