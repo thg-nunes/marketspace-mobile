97c66bb4fdc5ff16463a5677ef4bec6000d0af60
thiago <desenvolvedor.nunes@gmail.com>
2023-06-23 11:48:56 -0300

    chore: adiriona imagens de logo, alias para diretorio de assets e tipagem para imagens '.png'

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,19 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ["module-resolver", {
-        root: ["./src"],
-        alias: {
-          "@screens": "./src/screens",
-          "@components": "./src/components",
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+            '@components': './src/components'
+          }
         }
-      }]
+      ]
     ]
-  };
-};
+  }
+}
