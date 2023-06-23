377e6935dd7640bf82b3523d82df2b362f6c018c
thiago <desenvolvedor.nunes@gmail.com>
2023-06-23 14:31:24 -0300

    style: cria estilo dos botoes do app

---
+++ src/components/button/styled.ts
@@ -0,0 +1,42 @@
+import { TouchableOpacity } from 'react-native'
+import styled, { css } from 'styled-components/native'
+
+export type ButtonStyle = 'PRIMARY' | 'SECONDARY' | 'TERTIARY'
+
+type ButtonTypeStylePropsDTO = {
+  type: ButtonStyle
+}
+
+export const Container = styled(TouchableOpacity)<ButtonTypeStylePropsDTO>`
+  ${({ theme, type }) => css`
+    width: 100%;
+    max-width: 276px;
+    height: 42px;
+    max-height: 42px;
+
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    border-radius: 6px;
+    padding: 12px;
+
+    font-size: ${theme.font_size.md}px;
+    font-family: ${theme.font_familly.bold};
+
+    background: ${type === 'PRIMARY'
+      ? theme.colors.gray[300]
+      : type === 'SECONDARY'
+      ? theme.colors.gray[700]
+      : theme.colors.blue.light};
+  `}
+`
+
+export const ButtonText = styled.Text<ButtonTypeStylePropsDTO>`
+  ${({ theme, type }) => css`
+    color: ${type === 'PRIMARY'
+      ? theme.colors.gray[600]
+      : theme.colors.gray[100]};
+  `}
+`
