4d94cb182b891951bf0c8299c1a7583870d3c8d4
thiago <desenvolvedor.nunes@gmail.com>
2023-06-23 09:27:51 -0300

    feat: adiciona config base do estilo do app

---
+++ src/theme/index.tsx
@@ -0,0 +1,32 @@
+export const theme = {
+  colors: {
+    blue: {
+      dark: '#364D9D',
+      light: '#647AC7'
+    },
+    red: {
+      light: '#EE7979'
+    },
+    gray: {
+      100: '#F7F7F8',
+      200: '#EDECEE',
+      300: '#D9D8DA',
+      400: '#9F9BA1',
+      500: '#5F5B62',
+      600: '#3E3A40',
+      700: '#1A181B'
+    }
+  },
+  font_familly: {
+    400: 'Karla_400Regular',
+    700: 'Karla_700Bold'
+  },
+  lineHeight: '130%',
+  font_size: {
+    sm: '12px',
+    md: '14px',
+    lg: '16px',
+    xl: '20px',
+    '2xl': '24px'
+  }
+}
