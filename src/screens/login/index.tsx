d22de750d8d5ebc7b7001d1291ca27ea445ebcf2
thiago <desenvolvedor.nunes@gmail.com>
2023-06-23 14:51:55 -0300

    feat: adiciona parte de criar conta

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -3,6 +3,7 @@ import { Image, View } from 'react-native'
 import Logo from '@assets/logo/logo.png'
 
 import { Input } from '@components/input'
+import { Button } from '@components/button'
 
 import * as Styled from './styled'
 
@@ -11,19 +12,30 @@ type LoginProps = {}
 export const Login = ({}: LoginProps) => {
   return (
     <Styled.Container>
-      <Styled.LogoSection>
-        <Image source={Logo} />
-        <View style={{ alignItems: 'center' }}>
-          <Styled.Heading>marketspace</Styled.Heading>
-          <Styled.Description>Seu espaço de compra e venda</Styled.Description>
-        </View>
-      </Styled.LogoSection>
+      <Styled.Content>
+        <Styled.LogoSection>
+          <Image source={Logo} />
+          <View style={{ alignItems: 'center' }}>
+            <Styled.Heading>marketspace</Styled.Heading>
+            <Styled.Description>
+              Seu espaço de compra e venda
+            </Styled.Description>
+          </View>
+        </Styled.LogoSection>
 
-      <Styled.FormSection>
-        <Styled.FormHeader>Acesse sua conta</Styled.FormHeader>
-        <Input placeholder="E-mail" />
-        <Input isPassword placeholder="Senha" returnKeyType="send" />
-      </Styled.FormSection>
+        <Styled.FormSection>
+          <Styled.FormHeader>Acesse sua conta</Styled.FormHeader>
+          <Input placeholder="E-mail" />
+          <Input isPassword placeholder="Senha" returnKeyType="send" />
+        </Styled.FormSection>
+
+        <Button title="Entrar" type="TERTIARY" />
+      </Styled.Content>
+
+      <Styled.RegisterSection>
+        <Styled.FormHeader>Ainda não tem acesso?</Styled.FormHeader>
+        <Button title="Criar uma conta" type="PRIMARY" />
+      </Styled.RegisterSection>
     </Styled.Container>
   )
 }
