2c5c113888bd0040d173b853e9bfdf3840c2928d
thiago <desenvolvedor.nunes@gmail.com>
2023-06-24 18:48:10 -0300

    feat: criando screen de registro de usuario

---
+++ src/screens/register/index.tsx
@@ -0,0 +1,61 @@
+import { Image, ScrollView, View } from 'react-native'
+import { useTheme } from 'styled-components/native'
+
+import Logo from '@assets/logo/logo.png'
+
+import { Input } from '@components/input'
+import { Button } from '@components/button'
+import { UserPhoto } from '@components/userPhoto'
+
+import * as Styled from './styled'
+
+type RegisterProps = {}
+
+export const Register = ({}: RegisterProps) => {
+  const { colors } = useTheme()
+
+  return (
+    <Styled.Container>
+      <ScrollView
+        style={{ backgroundColor: colors.gray[200] }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={{ flex: 1 }}>
+          <Styled.Content>
+            <Styled.LogoSection>
+              <Image source={Logo} />
+              <View style={{ alignItems: 'center' }}>
+                <Styled.Heading>Boas vindas!</Styled.Heading>
+                <Styled.Description>
+                  Crie sua conta e use o espaço para comprar itens variados e
+                  vender seus produtos
+                </Styled.Description>
+              </View>
+            </Styled.LogoSection>
+
+            <UserPhoto />
+
+            <Styled.FormSection>
+              <Input placeholder="Nome" />
+              <Input placeholder="E-mail" />
+              <Input placeholder="Telefone" />
+              <Input isPassword placeholder="Senha" />
+              <Input
+                isPassword
+                placeholder="Confirmar Senha"
+                returnKeyType="send"
+              />
+            </Styled.FormSection>
+
+            <Button title="Criar" type="SECONDARY" />
+          </Styled.Content>
+
+          <Styled.RegisterSection>
+            <Styled.FormHeader>Já tem uma conta?</Styled.FormHeader>
+            <Button title="Ir para o login" type="PRIMARY" />
+          </Styled.RegisterSection>
+        </View>
+      </ScrollView>
+    </Styled.Container>
+  )
+}
