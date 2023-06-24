ab444404d43b7855b9e7fe15e2c64562ed4802cb
thiago <desenvolvedor.nunes@gmail.com>
2023-06-24 19:42:59 -0300

    feat: renderizando componente de rotas

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 import { ThemeProvider } from 'styled-components/native'
 import {
   useFonts,
   Karla_400Regular,
   Karla_700Bold
 } from '@expo-google-fonts/karla'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { theme } from './src/theme'
+import { Routes } from './src/routes'
 
 export default function App() {
   const [isFontLoading] = useFonts({ Karla_400Regular, Karla_700Bold })
 
   return (
-    <ThemeProvider theme={theme}>
-      <View>
-        <Text>App</Text>
-      </View>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      {isFontLoading && (
+        <ThemeProvider theme={theme}>
+          <StatusBar translucent style="dark" backgroundColor="transparent" />
+          <Routes />
+        </ThemeProvider>
+      )}
+    </SafeAreaProvider>
   )
 }
