7b690ed37251ca37bca3cd2b0778915bb4113872
thiago <desenvolvedor.nunes@gmail.com>
2023-06-30 13:31:51 -0300

    feat: implementando regra para trabalhar com tamanho dinamico da foto do usuario

--- src/components/userPhoto/index.tsx
+++ src/components/userPhoto/index.tsx
@@ -3,18 +3,29 @@ import { PencilSimpleLine, User } from 'phosphor-react-native'
 
 import * as Styled from './styled'
 
-type UserPhotoProps = {}
+type UserPhotoProps = {
+  size: Styled.Size
+  photoEdiIcontShow: boolean
+}
 
-export const UserPhoto = ({}: UserPhotoProps) => {
+const iconSize = {
+  xl: 44,
+  md: 28,
+  sm: 14
+}
+
+export const UserPhoto = ({ size, photoEdiIcontShow }: UserPhotoProps) => {
   const { colors } = useTheme()
 
   return (
-    <Styled.Container>
-      <User size={44} color={colors.gray[400]} />
+    <Styled.Container size={size}>
+      <User size={iconSize[size]} color={colors.gray[400]} />
 
-      <Styled.Pencel activeOpacity={0.9}>
-        <PencilSimpleLine size={16} color="#ffffff" />
-      </Styled.Pencel>
+      {photoEdiIcontShow && (
+        <Styled.Pencel activeOpacity={0.9}>
+          <PencilSimpleLine size={16} color="#ffffff" />
+        </Styled.Pencel>
+      )}
     </Styled.Container>
   )
 }
