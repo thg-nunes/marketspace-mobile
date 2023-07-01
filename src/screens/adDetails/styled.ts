7630cb8a54dd370318c1caf0a75542adbc86da5c
thiago <desenvolvedor.nunes@gmail.com>
2023-07-01 11:22:47 -0300

    style: iniciando construcao do estilo da screen de detalhes do anuncio

---
+++ src/screens/adDetails/styled.ts
@@ -0,0 +1,40 @@
+import { AntDesign } from '@expo/vector-icons'
+import styled, { css } from 'styled-components/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export const Container = styled(SafeAreaView)`
+  ${({ theme }) => css`
+    flex: 1;
+    background: ${theme.colors.gray[100]};
+  `}
+`
+
+export const GobackButton = styled.Pressable`
+  width: 28px;
+  max-width: 28px;
+  margin: 12px 24px;
+`
+
+export const GobackIcon = styled(AntDesign).attrs(({ theme }) => ({
+  name: 'arrowleft',
+  size: 24,
+  color: theme.colors.gray[700]
+}))``
+
+export const ImageCounter = styled.View<{ isActive: boolean }>`
+  ${({ theme, isActive }) => css`
+    width: 121px;
+    height: 3px;
+
+    opacity: ${isActive ? 100 / 100 : 50 / 100};
+    background: ${theme.colors.gray[100]};
+    border-radius: 9999px;
+  `}
+`
+
+export const ImageListContainer = styled.View`
+  ${({ theme }) => css`
+    position: relative;
+    width: 100%;
+  `}
+`
