63cf68b309fe479e7147903960a383e36bd542b1
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-07-03 14:22:16 -0300

    style: cria componente de text para reutilizar no app

---
+++ src/components/text/styled.ts
@@ -0,0 +1,21 @@
+import styled, { css } from 'styled-components/native'
+
+export type FontStyle = 'regurlar' | 'bold'
+export type FontColor = '100' | '200' | '300' | '400' | '500' | '600' | '700'
+export type FontSize = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl'
+
+type TextProps = {
+  font: FontStyle
+  size: FontSize
+  color: FontColor
+}
+
+export const Container = styled.Text<TextProps>`
+  ${({ theme, font, size, color }) => css`
+    color: ${theme.colors.gray[color]};
+    font-size: ${theme.font_size[size]}px;
+    font-family: ${font === 'regurlar'
+      ? theme.font_familly.regular
+      : theme.font_familly.bold};
+  `}
+`
