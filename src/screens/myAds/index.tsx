50288067451489bf85b3407ebdc457a45408d21b
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-07-03 15:34:24 -0300

    feat: cria screen de myAds para corrigir o erro de renderizacao

---
+++ src/screens/myAds/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+type MyAdsProps = {}
+
+export const MyAds = ({}: MyAdsProps) => {
+  return (
+    <View>
+      <Text> MyAds Screen </Text>
+    </View>
+  )
+}
