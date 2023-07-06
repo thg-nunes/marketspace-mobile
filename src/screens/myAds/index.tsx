7b1cc46702e56fa8d7f9049f06aa167cd420253a
thiago <desenvolvedor.nunes@gmail.com>
2023-07-06 10:27:46 -0300

    feat: criando renderizacao da quantidade e tipo de anuncio

--- src/screens/myAds/index.tsx
+++ src/screens/myAds/index.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
 import { Text } from '@components/text'
 import { Plus } from 'phosphor-react-native'
 import { useTheme } from 'styled-components'
+
+import { Select } from '@components/select'
 
 import * as Styled from './styled'
 
 export const MyAds = () => {
   const { colors } = useTheme()
+
+  const [selectIsOpen, setSelectIsOpen] = useState(false)
 
   return (
     <Styled.Container>
@@ -19,6 +24,15 @@ export const MyAds = () => {
         />
         <Plus size={24} color={colors.gray[700]} />
       </Styled.Header>
+
+      <Styled.MyAdsQuantity>
+        <Text text={`9 anúncios`} color="600" font="regular" size="md" />
+        <Select
+          items={['Todos', 'Ativos', 'Inativos']}
+          isOpen={selectIsOpen}
+          onPress={() => setSelectIsOpen(!selectIsOpen)}
+        />
+      </Styled.MyAdsQuantity>
     </Styled.Container>
   )
 }
