cac924a69a5a536444c779906e4a26ca6edd1773
thiago <desenvolvedor.nunes@gmail.com>
2023-07-06 16:20:25 -0300

    style: construindo style da screen de detalhes do anuncio

---
+++ src/screens/myAdDetails/styled.ts
@@ -0,0 +1,103 @@
+import { AntDesign } from '@expo/vector-icons'
+import styled, { css } from 'styled-components/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { PencilSimpleLine } from 'phosphor-react-native'
+
+export const Container = styled(SafeAreaView)`
+  ${({ theme }) => css`
+    flex: 1;
+    background: ${theme.colors.gray[200]};
+  `}
+`
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 12px 24px;
+`
+
+export const GobackIcon = styled(AntDesign).attrs(({ theme }) => ({
+  name: 'arrowleft',
+  size: 24,
+  color: theme.colors.gray[700]
+}))``
+
+export const EditAdIcon = styled(PencilSimpleLine).attrs(({ theme }) => ({
+  size: 24,
+  color: theme.colors.gray[700]
+}))``
+
+export const ImageCounter = styled.View<{ isActive: boolean }>`
+  ${({ theme, isActive }) => css`
+    width: 121px;
+    height: 3px;
+
+    opacity: ${isActive ? 100 / 100 : 50 / 100};
+    background: ${theme.colors.gray[100]};
+    border-radius: 9999px;
+  `}
+`
+
+export const ImageListContainer = styled.View`
+  position: relative;
+  width: 100%;
+`
+
+export const AmountIndicator = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.font_size.md}px;
+    font-family: ${theme.font_familly.bold};
+    color: ${theme.colors.blue.light};
+  `}
+`
+
+export const Amount = styled(AmountIndicator)`
+  ${({ theme }) => css`
+    font-size: ${theme.font_size.xl}px;
+    color: ${theme.colors.blue.light};
+  `}
+`
+
+export const PaymentMethod = styled.View`
+  flex: 1;
+  gap: 8px;
+  flex-direction: row;
+`
+
+export const RowCenterItems = styled.View`
+  gap: 8px;
+  align-items: center;
+  flex-direction: row;
+
+  padding-left: 24px;
+  padding-right: 24px;
+`
+
+export const ColumnCenterItems = styled(RowCenterItems)`
+  align-items: stretch;
+  padding-left: 24px;
+  padding-right: 24px;
+  padding-bottom: 17px;
+  flex-direction: column;
+`
+
+export const AdContent = styled.View`
+  flex: 1;
+  gap: 8px;
+  margin: 24px 0;
+
+  align-items: flex-start;
+
+  padding-left: 24px;
+  padding-right: 24px;
+`
+
+export const PaymentMethodsContainer = styled.View`
+  flex: 1;
+  gap: 12px;
+  margin-top: 16px;
+
+  padding-left: 24px;
+  padding-right: 24px;
+  padding-bottom: 26px;
+`
