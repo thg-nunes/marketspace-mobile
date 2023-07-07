2229150896976e27551f5c36416ee344d0998102
thiago <desenvolvedor.nunes@gmail.com>
2023-07-07 11:59:45 -0300

    style: criando componentes para selecionar os produtos

--- src/screens/adCreate/styled.ts
+++ src/screens/adCreate/styled.ts
@@ -1,6 +1,7 @@
 import { AntDesign } from '@expo/vector-icons'
 import styled, { css } from 'styled-components/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Plus } from 'phosphor-react-native'
 
 export const Container = styled(SafeAreaView)`
   ${({ theme }) => css`
@@ -21,4 +22,28 @@ export const GobackIcon = styled(AntDesign).attrs(({ theme }) => ({
   name: 'arrowleft',
   size: 24,
   color: theme.colors.gray[700]
+}))``
+
+export const ProductPhotoSelector = styled.TouchableOpacity`
+  width: 100px;
+  height: 100px;
+
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 6px;
+  background: ${({ theme }) => theme.colors.gray[300]};
+`
+
+export const ProductPhotoSelected = styled.Image`
+  width: 100px;
+  height: 100px;
+
+  border-width: 1px;
+  border-radius: 6px;
+`
+
+export const PlusIcon = styled(Plus).attrs(({ theme }) => ({
+  size: 24,
+  color: theme.colors.gray[400]
 }))``
