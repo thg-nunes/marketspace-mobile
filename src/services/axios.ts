17234ce827a238e196ff26dc15269c85c5c39b86
thiago <desenvolvedor.nunes@gmail.com>
2023-07-12 13:11:28 -0300

    feat: retornando erro tratado caso seja erro do client

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,9 +1,21 @@
-import axios from 'axios'
+import { AppError } from '@utils/screens/appError'
+import axios, { AxiosError } from 'axios'
 
 const baseURL = process.env.API_ENDPOINT // 'http://your_local_ip:3333'
 
 const api = axios.create({
   baseURL
 })
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    if (error.response && error.response.data) {
+      const errorResponse = error.response.data as AppError
+
+      return Promise.reject(new AppError(errorResponse))
+    }
+  }
+)
 
 export { api }
