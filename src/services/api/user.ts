298908e5e2410f287cb9d256e652b6493489315e
thiago <desenvolvedor.nunes@gmail.com>
2023-07-13 12:55:34 -0300

    feat: cria service para login do usuario

--- src/services/api/user.ts
+++ src/services/api/user.ts
@@ -1,8 +1,11 @@
+import { SignInParams } from '@hooks/login'
 import { api } from '@services/axios'
-import { UseHandleSubmitFormParams } from '@hooks/register'
 
 type UsersRoutes = {
   register: (data: FormData) => Promise<void>
+  signIn: (
+    data: SignInParams
+  ) => Promise<{ token: string; refresh_token: string }>
 }
 
 const usersRoutes: UsersRoutes = {
@@ -14,6 +17,18 @@ const usersRoutes: UsersRoutes = {
           'Content-Type': 'multipart/form-data'
         }
       })
+    } catch (error) {
+      throw error
+    }
+  },
+  signIn: async ({
+    email,
+    password
+  }: SignInParams): Promise<{ token: string; refresh_token: string }> => {
+    try {
+      const { data } = await api.post('/sessions', { email, password })
+
+      return data
     } catch (error) {
       throw error
     }
