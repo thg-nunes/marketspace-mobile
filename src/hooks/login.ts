1fc53677ce60352332d460b5b3075dac28b0118f
thiago <desenvolvedor.nunes@gmail.com>
2023-07-13 12:56:40 -0300

    feat: cria hook para logar o usuario envia-lo para o app

--- src/hooks/login.ts
+++ src/hooks/login.ts
@@ -1,17 +1,39 @@
-import { useNavigation } from '@react-navigation/native'
-import { NativeStackRoutesScreenProps } from '@routes/nativeStack.routes'
 import { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+import { NativeStackRoutesScreenProps } from '@routes/nativeStack.routes'
+import { usersRoutes } from '@services/api/user'
+
+import { myToast } from '@utils/toast'
+import { AppError } from '@utils/screens/appError'
+import { theme } from '../theme'
+
+export type SignInParams = {
+  email: string
+  password: string
+}
 
 function useHandleSignIn() {
   const { navigate } = useNavigation<NativeStackRoutesScreenProps>()
   const [isSign, setIsSign] = useState(false)
 
-  function handleSignIn() {
-    setIsSign(true)
-    setTimeout(() => {
+  async function handleSignIn({ email, password }: SignInParams) {
+    try {
+      setIsSign(true)
+
+      const response = await usersRoutes.signIn({ email, password })
+
       navigate('homeApp')
+    } catch (error) {
+      if (error instanceof AppError) {
+        myToast({
+          message: error.message,
+          background: theme.colors.red.light
+        })
+      }
+    } finally {
       setIsSign(false)
-    }, 500)
+    }
   }
 
   return {
