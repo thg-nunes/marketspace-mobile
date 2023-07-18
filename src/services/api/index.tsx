c3cd994fc9eb510339e8f8f69c24b1b8ed2803d4
thiago <desenvolvedor.nunes@gmail.com>
2023-07-18 13:27:20 -0300

    feat: cria service para cadastrar produto

--- src/services/api/index.tsx
+++ src/services/api/index.tsx
@@ -1,13 +1,22 @@
+import { ProductDTO } from '@dtos/product'
 import { api } from '@services/axios'
 
 type ApiServices = {
   fetchProducts: () => Promise<any>
+  createProduct: (product: ProductDTO) => Promise<string>
 }
 
 const apiServices: ApiServices = {
   fetchProducts: async (): Promise<any> => {
     const { data } = await api.get('/products')
     return data
+  },
+  createProduct: async (product: ProductDTO): Promise<string> => {
+    const {
+      data: { id }
+    } = await api.post<{ id: string }>('/products', product)
+
+    return id
   }
 }
 
