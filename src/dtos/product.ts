642e9705769374e4b2fcf8ef5e87990f1c4ca6a9
thiago <desenvolvedor.nunes@gmail.com>
2023-07-19 13:07:09 -0300

    chore: cria tipagem para os dados do anuncio

--- src/dtos/product.ts
+++ src/dtos/product.ts
@@ -6,3 +6,21 @@ export type ProductDTO = {
   accept_trade: boolean
   payment_methods: string[]
 }
+
+export type AdProductDTO = {
+  name: string
+  description: string
+  is_new: boolean
+  price: number
+  accept_trade: boolean
+  is_active: boolean
+  user_id: string
+  avatar: string
+  payment_methods: {
+    [key: string]: string
+    name: string
+  }[]
+  product_images: {
+    path: string
+  }[]
+}
