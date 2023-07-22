f14e92d2c27f19c43555359e070e57e5def72468
thiago <desenvolvedor.nunes@gmail.com>
2023-07-22 11:37:40 -0300

    feat: renderizando item na preview screen

--- src/screens/adPreview/index.tsx
+++ src/screens/adPreview/index.tsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react'
 import { ScrollView } from 'react-native'
 import { useTheme } from 'styled-components/native'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { FlatList, Image, View, ViewToken } from 'react-native'
 import {
   ArrowLeft,
@@ -31,16 +31,29 @@ import { UserPhoto } from '@components/userPhoto'
 import { PaymentMethod } from '@components/paymentMethod'
 
 import * as Styled from './styled'
+import { ProductDTO } from '@dtos/product'
+import { myToast } from '@utils/toast'
+import { theme } from '../../theme'
+import { apiServices } from '@services/api'
+import { BottomTabRoutesScreenProps } from '@routes/bottomTabs.routes'
+import { AppError } from '@utils/screens/appError'
+import { useFetchUserStorageData } from '@hooks/home'
+import { api } from '@services/axios'
+import { productPaymentChecked } from '@utils/screens/adCreate'
 
 export const AdPreview = () => {
   const { colors } = useTheme()
   const { goBack } = useNavigation<NativeStackRoutesScreenProps>()
+  const bottomNavigation = useNavigation<BottomTabRoutesScreenProps>()
+
+  const { params } = useRoute()
+  const { images, product } = params as {
+    images: string[]
+    product: ProductDTO
+  }
+  const { userData } = useFetchUserStorageData()
 
   const [activeImage, setActiveImage] = useState(0)
-  const images = [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3PFe0VM926Vss_eH-gkyWaUn3HUkkqeBxrw&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAMB7VSqy-EsGhPHiNV1ac6KIJo5e1eudaLw&usqp=CAU'
-  ]
 
   function onViewableItemsChanged(info: {
     viewableItems: Array<ViewToken>
@@ -48,10 +61,6 @@ export const AdPreview = () => {
   }) {
     const imageIndex = info.changed[0]?.index as number
     setActiveImage(imageIndex)
-  }
-
-  function handleGobackScreen() {
-    goBack()
   }
 
   const viewabilityConfig = {
@@ -64,6 +73,29 @@ export const AdPreview = () => {
       onViewableItemsChanged
     }
   ])
+
+  async function handleAdCreate() {
+    try {
+      const product_id = await apiServices.createProduct(product)
+
+      images.forEach(
+        async (image) => await apiServices.createProductImage(product_id, image)
+      )
+
+      myToast({
+        message: 'Produto cadastrado com sucesso.',
+        background: theme.colors.green.dark
+      })
+      setTimeout(() => bottomNavigation.navigate('myAds'), 1500)
+    } catch (error) {
+      if (error instanceof AppError) {
+        myToast({
+          message: error.message,
+          background: theme.colors.red.light
+        })
+      }
+    }
+  }
 
   return (
     <Styled.Container>
@@ -121,18 +153,26 @@ export const AdPreview = () => {
       >
         <View>
           <RowCenterItems>
-            <UserPhoto size="sm" photoEdiIcontShow={false} />
-            <Text text="Maria" color="700" font="regular" size="md" />
+            <UserPhoto.Root
+              size="sm"
+              uri={`${api.defaults.baseURL}/images/${userData.avatar}`}
+            />
+            <Text text={userData.name} color="700" font="regular" size="md" />
           </RowCenterItems>
 
           <AdContent>
             <Tag.Root type="GRAY.300" size="sm">
-              <Text text="NOVO" color="600" font="bold" size="xsm" />
+              <Text
+                text={product.is_new ? 'NOVO' : 'USADO'}
+                color="600"
+                font="bold"
+                size="xsm"
+              />
             </Tag.Root>
 
             <View style={{ flexDirection: 'row' }}>
               <Text
-                text="Bicicleta"
+                text={product.name}
                 color="700"
                 font="bold"
                 size="xl"
@@ -140,14 +180,11 @@ export const AdPreview = () => {
               />
               <View style={{ flexDirection: 'row' }}>
                 <AmountIndicator>R$</AmountIndicator>
-                <Amount>129,90</Amount>
+                <Amount>{product.price}</Amount>
               </View>
             </View>
             <Text
-              text="Cras congue cursus in tortor sagittis placerat nunc, tellus arcu.
-              Vitae ante leo eget maecenas urna mattis cursus. Mauris metus amet
-              nibh mauris mauris accumsan, euismod. Aenean leo nunc, purus
-              iaculis in aliquam."
+              text={product.description}
               color="600"
               font="regular"
               size="md"
@@ -156,7 +193,12 @@ export const AdPreview = () => {
 
           <RowCenterItems>
             <Text text="Aceita troca?" size="md" font="bold" color="600" />
-            <Text text="Sim" size="md" font="regular" color="600" />
+            <Text
+              text={product.accept_trade ? 'Sim' : 'Não'}
+              size="md"
+              font="regular"
+              color="600"
+            />
           </RowCenterItems>
 
           <PaymentMethodsContainer>
@@ -166,30 +208,55 @@ export const AdPreview = () => {
               font="bold"
               color="700"
             />
-            <PaymentMethod.Root>
-              <PaymentMethod.Icon Icon={Barcode} />
-              <PaymentMethod.Type type="Boleto" />
-            </PaymentMethod.Root>
+            {productPaymentChecked({
+              productAcceptPayments: product.payment_methods,
+              paymentType: 'boleto'
+            }) && (
+              <PaymentMethod.Root>
+                <PaymentMethod.Icon Icon={Barcode} />
+                <PaymentMethod.Type type="Boleto" />
+              </PaymentMethod.Root>
+            )}
 
-            <PaymentMethod.Root>
-              <PaymentMethod.Icon Icon={QrCode} />
-              <PaymentMethod.Type type="Pix" />
-            </PaymentMethod.Root>
+            {productPaymentChecked({
+              productAcceptPayments: product.payment_methods,
+              paymentType: 'pix'
+            }) && (
+              <PaymentMethod.Root>
+                <PaymentMethod.Icon Icon={QrCode} />
+                <PaymentMethod.Type type="Pix" />
+              </PaymentMethod.Root>
+            )}
 
-            <PaymentMethod.Root>
-              <PaymentMethod.Icon Icon={Money} />
-              <PaymentMethod.Type type="Dinheiro" />
-            </PaymentMethod.Root>
+            {productPaymentChecked({
+              productAcceptPayments: product.payment_methods,
+              paymentType: 'cash'
+            }) && (
+              <PaymentMethod.Root>
+                <PaymentMethod.Icon Icon={Money} />
+                <PaymentMethod.Type type="Dinheiro" />
+              </PaymentMethod.Root>
+            )}
 
-            <PaymentMethod.Root>
-              <PaymentMethod.Icon Icon={CreditCard} />
-              <PaymentMethod.Type type="Cartão de Crédito" />
-            </PaymentMethod.Root>
+            {productPaymentChecked({
+              productAcceptPayments: product.payment_methods,
+              paymentType: 'card'
+            }) && (
+              <PaymentMethod.Root>
+                <PaymentMethod.Icon Icon={CreditCard} />
+                <PaymentMethod.Type type="Cartão de Crédito" />
+              </PaymentMethod.Root>
+            )}
 
-            <PaymentMethod.Root>
-              <PaymentMethod.Icon Icon={Bank} />
-              <PaymentMethod.Type type="Depósito Bancário" />
-            </PaymentMethod.Root>
+            {productPaymentChecked({
+              productAcceptPayments: product.payment_methods,
+              paymentType: 'deposit'
+            }) && (
+              <PaymentMethod.Root>
+                <PaymentMethod.Icon Icon={Bank} />
+                <PaymentMethod.Type type="Depósito Bancário" />
+              </PaymentMethod.Root>
+            )}
           </PaymentMethodsContainer>
         </View>
       </ScrollView>
@@ -206,7 +273,7 @@ export const AdPreview = () => {
         <Button.Root
           type="PRIMARY"
           style={{ maxWidth: '100%' }}
-          onPress={handleGobackScreen}
+          onPress={goBack}
         >
           <Button.Icon
             Icon={ArrowLeft}
@@ -218,7 +285,11 @@ export const AdPreview = () => {
           <Text color="600" font="bold" size="md" text="Voltar e editar" />
         </Button.Root>
 
-        <Button.Root type="SECONDARY" style={{ maxWidth: '100%' }}>
+        <Button.Root
+          type="SECONDARY"
+          style={{ maxWidth: '100%' }}
+          onPress={handleAdCreate}
+        >
           <Button.Icon
             Icon={TagIcon}
             iconProps={{
