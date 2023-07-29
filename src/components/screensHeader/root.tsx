3e0f1a66e48263323b2bef109e875bdadc2fb7b3
thiago <desenvolvedor.nunes@gmail.com>
2023-07-29 13:39:00 -0300

    refactor: adiciona possibilidade de passar estilo de espacamento

--- src/components/screensHeader/root.tsx
+++ src/components/screensHeader/root.tsx
@@ -3,12 +3,30 @@ import { View } from 'react-native'
 
 type RootScreensHeaderProps = {
   children: ReactNode
+  mb?: number
+  mt?: number
+  pl?: number
+  pr?: number
 }
 
-export const RootScreensHeader = ({ children }: RootScreensHeaderProps) => {
+export const RootScreensHeader = ({
+  children,
+  mb = 0,
+  mt = 0,
+  pl = 0,
+  pr = 0
+}: RootScreensHeaderProps) => {
   return (
     <View
-      style={{ marginBottom: 36, flexDirection: 'row', alignItems: 'center' }}
+      style={{
+        marginTop: mt,
+        marginBottom: mb,
+        paddingLeft: pl,
+        paddingRight: pr,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+      }}
     >
       {children}
     </View>
